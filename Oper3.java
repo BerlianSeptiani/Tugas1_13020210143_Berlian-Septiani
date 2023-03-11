c2c71b8c553bb3450416807eb10e4b6328447a17
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-11 22:56:17 +0800

    Add files via upload

---
+++ Oper3.java
@@ -0,0 +1,20 @@
+/* Nama : Berlian Septiani
+   Nim : 13020210143
+   Hari/Tanggal :Rabu,08 Maret 2023
+   Waktu Pengerjaan : 11.50 */
+
+
+public class Oper3 {
+    public static void main (String[] asgr) {
+           
+       if (true && true){ System.out.println(true && true);}
+           /* true = true and true */
+       if (true & true) { System.out.println(true & false); }
+           /* true & true */
+       if (true){ System.out.println(true); }/* true */
+       if (true || true){ System.out.println(true);}
+          /* true = true or true */
+       if (true|false) { System.out.println(true|false); }
+          /* true|false */
+     }
+}
---
+++ Oper4.java
@@ -0,0 +1,28 @@
+/* Nama : Berlian Septiani
+   Nim : 13020210143
+   Hari/Tanggal :Rabu,08 Maret 2023
+   Waktu Pengerjaan : 11.53 */
+
+
+/* Operator terner */
+
+public class Oper4 {
+   
+    public static void main(String[] args) {
+    
+         int i = 0;         /* perhatikan int i,j=0 bukan seperti ini */
+         int j = 0;
+         char c = 8; char d = 10;
+         int e = (((int)c > (int)d) ? c: d);
+         int k = ((i>j) ? i: j);
+
+
+/* ALGORITMA */
+       System.out.print ("Nilai e = "+ e); 
+       System.out.print ("\nNilai k = "+ k);
+              i = 2;
+              j = 3;
+              k = ((i++>j++) ? i: j) ;
+       System.out.print ("\nNilai k = "+ k);
+        }
+}
