a4de6dba7ca1186ad27b2c3bd04c0ca66286012f
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-11 22:49:19 +0800

    Add files via upload

---
+++ Incr.java
@@ -0,0 +1,17 @@
+/* Nama : Berlian Septiani
+   Nim : 13020210143
+   Hari/Tanggal :Rabu,08 Maret 2023
+   Waktu Pengerjaan : 11.24 */
+
+
+/* Effek dari operator ++ */
+
+
+public class Incr {
+   public static void main (String [] args) {
+     int i,j;
+     i = 3;
+     j = i++;
+System.out.println ("Nilai 1 : " + (++i) + "\nNilai j : " + j);
+    }
+}
