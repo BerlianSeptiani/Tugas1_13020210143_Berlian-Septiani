06d7e0be7636374bb44b226a992c6e5f83a3b746
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-11 22:48:44 +0800

    Add files via upload

---
+++ Ekspresi.java
@@ -0,0 +1,25 @@
+/* Nama : Berlian Septiani
+   Nim : 13020210143
+   Hari/Tanggal :Rabu,08 Maret 2023
+   Waktu Pengerjaan : 10.14 */
+
+/*pemakain operator kondisional*/
+
+public class Ekspresi {
+   
+   public static void main (String [] args) {
+       int x = 1;
+       int y = 2;
+   
+   System.out.print ("x = "+ x + "\n");
+   System.out.print ("y = "+ y + "\n");
+   System.out.print ("hasil ekspresi = (x<y) ? = "
+   + ((x < y) ? "true" : "false"));
+
+/*Gunakan dalam kurung "(statemen dan kondisi)"
+ untuk menyatakan satu kesatuan pernyataan*/
+
+   }
+
+}
+   
