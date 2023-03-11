2c611fbe7787aa9e967221a0ea1a0a84c81cbde4
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-11 23:14:00 +0800

    Add files via upload

---
+++ KonversiWaktu.java
@@ -0,0 +1,57 @@
+ /* Nama : Berlian Septiani
+   Nim  : 13020210143
+   Hari/Tanggal : 11 Maret 2023
+   Waktu Pengerjaan : 23.03 *\
+
+
+package konversiwaktu;
+
+
+import java.util.Scanner;
+
+public class KonversiWaktu {
+   
+ public static void main(String[] args) {
+  
+ 
+     Scanner input = new Scanner(System.in);
+
+  
+    
+  int totJam, jam, totMen, menit, detik, konversi;
+
+    
+    System.out.print("Masukkan total detik : ");
+       
+ konversi = input.nextInt();
+
+ 
+     
+  detik = konversi % 60;
+      
+  totMen = konversi / 60;
+      
+  menit = totMen % 60;
+     
+   totJam = totMen / 60;
+      
+  jam = totJam % 24;
+  
+      
+      
+  System.out.println("Detik sekarang  : " +detik+ " detik");
+      
+  System.out.println("Total menit     : " +totMen+ " menit");
+     
+   System.out.println("Menit sekarang  : " +menit+ " menit");
+        
+System.out.println("Total jam       : " +totJam+ " jam");
+       
+ System.out.println("Jam sekarang    : " +jam+ " jam");
+      
+  System.out.println("Tampil waktu    : " + jam + ":" + menit + ":" + detik);
+ 
+   }
+    
+
+}
