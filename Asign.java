361a5f415466bab84160b8abdf687f6b7c417b0b
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-11 22:47:27 +0800

    Add files via upload

---
+++ Asign.java
@@ -0,0 +1,16 @@
+/* Nama : Berlian Septiani
+   Nim  : 13020210143
+   Hari/Tanggal : 07 Maret 2023
+   Waktu Pengerjaan : 23.36 *\
+
+
+
+public class Asign {
+   
+   public static void main (String [] args) {
+         int i;
+       
+      System.out.print ("hello\n"); i = 5;
+      System.out.println ("Ini nilai i : " + i);
+    }
+}
